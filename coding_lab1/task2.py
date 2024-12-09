9271db880f3c0721a7b97ad8789d257c6128452d
MikeCynic <michaelshmidtt@gmail.com>
2024-12-09 18:34:21 +0300

    Add files via upload

---
+++ coding_lab1/task2.py
@@ -0,0 +1,21 @@
+from task_1 import Physicist, Bodybuilder, Polyglot  #не видит импортированные классы, не понимаю почему
+
+
+student1 = Physicist('none', 'something')
+gymbro = Bodybuilder(19, 'none', 100)
+adonis = Polyglot(10, 80)
+if __name__ == "__main__":
+    try:
+        student1.get_a_life(1)
+    except TypeError:
+        print('Ошибка: неправильные данные')
+
+    try:
+        gymbro.get_injured(1)
+    except TypeError:
+        print('Ошибка: неправильные данные')
+
+    try:
+     adonis.acquire_a_language(1)
+    except TypeError:
+        print('Ошибка: неправильные данные')
